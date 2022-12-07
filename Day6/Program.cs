279a1dafd2efa6d2f82b1558fd2183c83e1ebabd
robert <robert.sargeus@geshdo.com>
2022-12-07 21:42:34 +0100

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+string inputtext = System.IO.File.ReadAllText(@"input.txt");
+string[] values = inputtext.Split(new[] { "\n" }, StringSplitOptions.None);
+
+
+string datastream = values[0];
+
+int offset = 14;
+
+for (int i = 0; i < datastream.Length - offset; i++)
+{
+    var msg = datastream.Substring(i, offset);
+
+    int distinct = msg.Distinct().Count();
+
+    Console.WriteLine(distinct + " " + msg + ":" + (i+offset));
+    if (distinct == offset)
+    {
+        break;
+    }
+}
